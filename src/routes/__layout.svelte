2f49ac185e2ab336c39bdb2edc5bdcac46d73e08
Carlos Yeric Fonseca Rios <yeric17@gmail.com>
2022-01-11 00:52:10 -0500

    add footer for pages main layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -30,7 +30,21 @@
 <Header></Header>
 
 <main class="main">
-    <slot></slot>
+    <div class="main_section">
+        <slot></slot>
+    </div>
+    <footer class="footer">
+        <div class="footer_container">
+            <div class="footer_nav">
+                <ul class="nav_list">
+                    <li class="nav_list_item"><a href="/contact" target="_blank">Contacto</a></li>
+                    <li class="nav_list_item"><a href="https://www.linkedin.com/in/yericfonseca" target="_blank">Linkedin</a></li>
+                    <li class="nav_list_item"><a href="https://twitter.com/yeric17" target="_blank">Twitter</a></li>
+                    <li class="nav_list_item"><a href="https://github.com/yeric17" target="_blank">Github</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </main>
 <div class="notifications-container">
     {#if $notifications.length > 0}
@@ -47,6 +61,13 @@
 </div>
 
 <style>
+    .main{
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr auto;
+        min-height: 100%;
+    }
+
     .notifications-container {
 		position: fixed;
 		flex-direction: column;
@@ -57,4 +78,27 @@
 		justify-content: center;
 		align-items: center;
 	}
+
+    .footer {
+        background-color: var(--color-secondary-dark);
+        color:white;
+        padding-top: var(--spacing-xxl);
+        padding-bottom: var(--spacing-xxl);
+        padding-left: var(--spacing-sm);
+        padding-right: var(--spacing-sm);
+    }
+    .footer_container {
+        max-width: var(--max-width);
+        margin: 0 auto;
+    }
+
+    .nav_list {
+        list-style: none;
+        display: flex;
+        align-items: center;
+        gap: var(--spacing-md);
+    }
+    .nav_list_item:hover {
+        text-decoration: underline;
+    }
 </style>
