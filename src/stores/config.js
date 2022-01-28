00de33762b89e3fe4fbaacaf304b45dfdc3155be
Carlos Yeric Fonseca Rios <yeric17@gmail.com>
2022-01-28 00:22:38 -0500

    change config file API_HOST

--- src/stores/config.js
+++ src/stores/config.js
@@ -1,5 +1,6 @@
 const MODE = import.meta.env.VITE_MODE;
-const API_HOST = MODE === 'dev'? import.meta.env.VITE_API_HOST_DEV : import.meta.env.VITE_API_HOST_PROD;
+// const API_HOST = MODE === 'dev'? import.meta.env.VITE_API_HOST_DEV : import.meta.env.VITE_API_HOST_PROD;
+const API_HOST = "https://yeric-blog-api.herokuapp.com:7070";
 
 
 
