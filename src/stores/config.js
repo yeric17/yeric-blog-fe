ff5ebe06e3f74135dbef09f1b9cc5eb5df20e94a
Carlos Yeric Fonseca Rios <yeric17@gmail.com>
2022-05-22 23:55:31 -0500

    test prod

--- src/stores/config.js
+++ src/stores/config.js
@@ -1,4 +1,4 @@
-const MODE = "dev";
+const MODE = "prod";
 // const API_HOST = MODE === 'dev'? import.meta.env.VITE_API_HOST_DEV : import.meta.env.VITE_API_HOST_PROD;
 const API_HOST = MODE === 'dev'? "http://localhost:3000/api" : "https://yericdev.herokuapp.com/api";
 
