f0e5d2bf0f308cf504238a470035bd2a5f50dfea
Carlos Yeric Fonseca Rios <yeric17@gmail.com>
2022-05-23 19:22:12 -0500

    update Gallery component

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -9,13 +9,8 @@
 <style>
     .gallery {
         display: grid;
-<<<<<<< HEAD
         grid-template-columns: repeat(auto-fit, minmax(var(--minwidth), 1fr));
-        grid-gap: 1rem;
-=======
-        grid-template-columns: repeat(auto-fit, minmax(var(--maxwidth), 1fr));
         grid-gap: 2rem;
->>>>>>> 1dd47f86c3987095b748c5efb0b540fe6a31fbbf
         justify-items: center;
     }
 </style>
